e8a7db8cf78369b25a381423b2670f451bf9698c
Samuel Racak <samo.racak@gmail.com>
2023-04-10 21:09:09 +0200

    added premium user

---
+++ src/user/PremiumUser.java
@@ -0,0 +1,15 @@
+package user;
+
+import java.time.LocalDate;
+
+import car.Car;
+
+public class PremiumUser extends User {
+    private PaymentCard card;
+
+    public PremiumUser(UserType userType, String name, String email, String password, LocalDate dataOfBirth,
+            String gender, Car car, PaymentCard card) {
+        super(userType, name, email, password, dataOfBirth, gender, car);
+        this.card = card;
+    }
+}
