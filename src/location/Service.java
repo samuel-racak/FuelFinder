6b8119fe550d1af5f348c58c54a5523fd93a7da2
Samuel Racak <samo.racak@gmail.com>
2023-04-10 22:34:14 +0200

    added service class

---
+++ src/location/Service.java
@@ -0,0 +1,37 @@
+package location;
+
+public class Service {
+    private String serviceName;
+    private int price;
+
+    public Service(String serviceName, int price) {
+        this.serviceName = serviceName;
+        this.price = price;
+    }
+
+    public String getServiceName() {
+        return this.serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Service serviceName(String serviceName) {
+        setServiceName(serviceName);
+        return this;
+    }
+
+    public Service price(int price) {
+        setPrice(price);
+        return this;
+    }
+}
