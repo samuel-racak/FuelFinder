d4160d230c1fde5cf69a1d6c34398abd34244ada
Samuel Racak <samo.racak@gmail.com>
2023-04-10 21:09:28 +0200

    created a new payment card class

---
+++ src/user/PaymentCard.java
@@ -0,0 +1,54 @@
+package user;
+
+public class PaymentCard {
+    private String cardNumber;
+    private String expirationDate;
+    private String securityCode;
+
+    public PaymentCard(String cardNumber, String expirationDate, String securityCode) {
+        if (isValid(cardNumber)) {
+            this.cardNumber = cardNumber;
+            this.expirationDate = expirationDate;
+            this.securityCode = securityCode;
+        } else {
+            // TODO handle this situation
+            System.out.print("wrong security code");
+        }
+    }
+
+    /**
+     * Takes cardNumber and checks if card is valid
+     * Luhns algorithm made see the
+     * <a href="https://en.wikipedia.org/wiki/Luhn_algorithm"</a>
+     *
+     * @param cardNumber
+     * @return true if valid, else false
+     */
+    public static boolean isValid(String cardNumber) {
+        int sum = 0;
+        int parity = cardNumber.length() % 2;
+        for (int i = 0; i < cardNumber.length(); i++) {
+            int digit = Character.getNumericValue(cardNumber.charAt(i));
+            if (i % 2 != parity) {
+                sum += digit;
+            } else if (digit > 4) {
+                sum += 2 * digit - 9;
+            } else {
+                sum += 2 * digit;
+            }
+        }
+        return sum % 10 == 0;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public String getSecurityCode() {
+        return securityCode;
+    }
+}
