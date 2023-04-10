fbdba78610d965f7b9a16415289aaeba22a4549d
Samuel Racak <samo.racak@gmail.com>
2023-04-10 19:49:44 +0200

    added other user attributes

--- src/user/User.java
+++ src/user/User.java
@@ -2,24 +2,34 @@ package user;
 
 import java.time.LocalDate;
 
+import car.Car;
+
 public class User {
 
     private String name;
     private String email;
     private String password;
     private LocalDate dateOfBirth;
+    private String gender;
+    private Car car;
 
     /**
+     * this is a basic user account created by UserManager
+     * 
      * @param name
      * @param email
      * @param password
      * @param dataOfBirth
+     * @param gender
+     * @param car
      */
-    public User(String name, String email, String password, LocalDate dataOfBirth) {
+    public User(String name, String email, String password, LocalDate dataOfBirth, String gender, Car car) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.car = car;
     }
 
     public String getName() {
@@ -52,5 +62,21 @@ public class User {
 
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
     }
 }
