6b8b6e0fa1956b53fbb1a1b99dd175bade3cb068
Samuel Racak <samo.racak@gmail.com>
2023-04-10 23:26:12 +0200

    added location class which represents exact location on a "map"

---
+++ src/location/Location.java
@@ -0,0 +1,38 @@
+package location;
+
+// this class will remember on which route we are and what is our actual position which will be calculated from the length of the route
+public class Location {
+    private Route currentRoute;
+    private int exactKilometer;
+
+    public Location(Route currentRoute, int exactKilometer) {
+        this.currentRoute = currentRoute;
+        this.exactKilometer = exactKilometer;
+    }
+
+    public Route getCurrentRoute() {
+        return this.currentRoute;
+    }
+
+    public void setCurrentRoute(Route currentRoute) {
+        this.currentRoute = currentRoute;
+    }
+
+    public int getExactKilometer() {
+        return this.exactKilometer;
+    }
+
+    public void setExactKilometer(int exactKilometer) {
+        this.exactKilometer = exactKilometer;
+    }
+
+    public Location currentRoute(Route currentRoute) {
+        setCurrentRoute(currentRoute);
+        return this;
+    }
+
+    public Location exactKilometer(int exactKilometer) {
+        setExactKilometer(exactKilometer);
+        return this;
+    }
+}
