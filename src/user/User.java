450cf6f669b9ec71d5de8d89db8e07e05beab20c
Samuel Racak <samo.racak@gmail.com>
2023-04-10 19:32:41 +0200

    changed format to localDate(easier) + added getters and setters

--- src/user/User.java
+++ src/user/User.java
@@ -5,13 +5,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Date;
+import java.io.Serializable;
+import java.time.LocalDate;
 
-public class User {
+public class User implements Serializable {
     private String name;
     private String email;
     private String password;
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
 
     /**
      * @param name
@@ -19,10 +20,42 @@ public class User {
      * @param password
      * @param dataOfBirth
      */
-    public User(String name, String email, String password, Date dataOfBirth) {
+    public User(String name, String email, String password, LocalDate dataOfBirth) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
@@ -54,4 +87,5 @@ public class User {
         }
         return user;
     }
+
 }
