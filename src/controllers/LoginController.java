7ea6863d62684831c723329111d31f2a1fe0e6ad
Samuel Racak <samo.racak@gmail.com>
2023-05-13 14:38:36 +0200

    added multithreading to LoginController

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -28,12 +30,41 @@ public class LoginController extends BasicController {
         String password = passwordTextField.getText();
         // call some other thread to check if login info correct ? y -> move to next
         // scene
-        System.out.println("handle login");
-        System.out.println("----------");
-        System.out.println(userName);
-        System.out.println(password);
-        System.out.println("----------");
-        // TODO handle login
+
+        if (userName.isEmpty() || password.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Login Error");
+            alert.setContentText("Please enter username and password");
+            alert.showAndWait();
+        } else {
+            Task<Boolean> loginTask = new Task<>() {
+                @Override
+                protected Boolean call() {
+                    return sessionManager.login(userName, password);
+                }
+            };
+
+            loginTask.setOnSucceeded(otherEvent -> {
+                if (loginTask.getValue()) {
+                    System.out.println("user logged in");
+                    windowManager.switchToScene("primaryStage", "mainScene");
+                    // clear text fields
+                    userNameTextField.clear();
+                    passwordTextField.clear();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Login Error");
+                    alert.setContentText("Username or password incorrect");
+                    alert.showAndWait();
+                }
+            });
+
+            Thread thread = new Thread(loginTask);
+            thread.setDaemon(true);
+            thread.start();
+        }
     }
 
     @FXML
@@ -44,10 +75,37 @@ public class LoginController extends BasicController {
 
     @FXML
     public void goToAdminPage(ActionEvent event) {
-        System.out.println("user wants to go to admin page");
-        windowManager.switchToScene("primaryStage", "adminScene");
-        // TODO: check if user is admin
-        // windowManager.switchToScene("primaryStage", "adminScene");
+        String userName = userNameTextField.getText();
+        String password = passwordTextField.getText();
+
+        if (userName.isEmpty() || password.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Login Error");
+            alert.setContentText("Please enter username and password");
+            alert.showAndWait();
+        } else {
+            if (sessionManager.login(userName, password)) {
+                System.out.println("user logged in");
+                // clear text fields
+                if (sessionManager.isAdmin()) {
+                    windowManager.switchToScene("primaryStage", "adminScene");
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Admin Error");
+                    alert.setContentText("You are not an admin");
+                    alert.showAndWait();
+                }
+
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Login Error");
+                alert.setContentText("Username or password incorrect");
+                alert.showAndWait();
+            }
+        }
     }
 
     @Override
