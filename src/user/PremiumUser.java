19dec3a3f667257dfe60752f20be6012f6d826b3
Samuel Racak <samo.racak@gmail.com>
2023-05-13 17:37:37 +0200

    added method to getCard

--- src/user/PremiumUser.java
+++ src/user/PremiumUser.java
@@ -7,11 +7,14 @@ import car.Car;
 public class PremiumUser extends User {
     private PaymentCard card;
 
-    public PremiumUser(UserType userType, String name, String email, String password, LocalDate dataOfBirth,
+    public PremiumUser(UserType userType, String userName, String email, String password, LocalDate dataOfBirth,
             String gender, Car car, PaymentCard card) {
-        super(userType, name, email, password, dataOfBirth, gender, car);
+        super(userType, userName, email, password, dataOfBirth, gender, car);
         this.card = card;
     }
 
+    public PaymentCard getCard() {
+        return card;
+    }
     // TODO add some methods for premium user
 }
