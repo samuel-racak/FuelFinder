ab23920882391cb8614aed6d9130376e26cbddf8
Samuel Racak <samo.racak@gmail.com>
2023-05-14 11:06:07 +0200

    finished goPremium

--- src/controllers/PremiumUpgradeController.java
+++ src/controllers/PremiumUpgradeController.java
@@ -1,34 +1,61 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-import user.PaymentCard;
+import javafx.stage.Stage;
 
 public class PremiumUpgradeController extends BasicController {
     @FXML
     private TextField cardNumberField;
+
     @FXML
-    private TextField expirationDateField;
+    private DatePicker cardExpirationDate;
+
     @FXML
     private TextField securityCodeField;
 
     @FXML
     private void handleSubmitButtonAction() {
-        System.out.println("Submit button clicked!");
+        if (cardNumberField.getText().isEmpty() || cardExpirationDate.getValue() == null
+                || securityCodeField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Invalid card details");
+            alert.setContentText("Please fill all the fields and try again");
+            alert.showAndWait();
+            return;
+        }
 
-        // Get the entered values from the form fields
-        String cardNumber = cardNumberField.getText();
-        String expirationDate = expirationDateField.getText();
-        String securityCode = securityCodeField.getText();
+        if (!sessionManager.changeToPremium(sessionManager.getCurrentUser(), cardNumberField.getText(),
+                cardExpirationDate.getValue(), securityCodeField.getText())) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Invalid card details");
+            alert.setContentText("Please check your card details and try again");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("You are now a premium user");
+            alert.setContentText("You can now enjoy all the premium features");
+            alert.showAndWait();
+        }
 
-        // Create a new PaymentCard object with the entered values
-        PaymentCard card = new PaymentCard(cardNumber, expirationDate, securityCode);
-
-        // TODO: Do something with the created PaymentCard object
+        Stage stage = (Stage) cardNumberField.getScene().getWindow(); // get the current stage
+        stage.close(); // close the current window
     }
 
     @Override
     public void setTitle() {
         stage.setTitle("Premium Upgrade");
+    }
+
+    @Override
+    public void fillGUI() {
+        cardNumberField.clear();
+        cardExpirationDate.setValue(null);
+        securityCodeField.clear();
     }
 }
