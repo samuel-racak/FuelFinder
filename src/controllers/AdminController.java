9cf48169c8aa434d33e05efe9d9da7193817ac8c
Samuel Racak <samo.racak@gmail.com>
2023-05-14 11:05:49 +0200

    finished admin

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,13 +1,17 @@
 package controllers;
 
+import exceptions.noPermissionException;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 import user.User;
+import user.UserType;
 
 public class AdminController extends BasicController {
 
@@ -21,7 +25,7 @@ public class AdminController extends BasicController {
     private Button logoutButton;
 
     @FXML
-    private Label selectedUser;
+    private Label selectedUserLabel;
 
     @FXML
     private Button setAdminButton;
@@ -50,35 +54,161 @@ public class AdminController extends BasicController {
 
         // Set the clip property of the ImageView to the Circle
         imageView.setClip(clip);
-    }
 
-    @FXML
-    void selectUser(ActionEvent event) {
-        if (userList.getSelectionModel().getSelectedItem() != null) {
-            userNameLabel.setText(userList.getSelectionModel().getSelectedItem().getUserName());
-        }
+        userList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                // Update the label with the selected user's information
+                selectedUserLabel.setText(newValue.getUserName());
+            } else {
+                // Clear the label if no user is selected
+                selectedUserLabel.setText("");
+            }
+        });
+
     }
 
     @FXML
     void deleteUser(ActionEvent event) {
-        // TODO: Delete user
+        User toDelete = userList.getSelectionModel().getSelectedItem();
+
+        if (toDelete == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No user selected");
+            alert.setContentText("Please select a user to delete");
+            alert.showAndWait();
+        }
+
+        if (toDelete != null) {
+            System.out.println(sessionManager.getCurrentUsername() + " is deleting " + toDelete.getUserName());
+            System.out.println("Current user is " + sessionManager.getCurrentUser().getUserType());
+
+            Task<Boolean> deleteTask = new Task<>() {
+                @Override
+                protected Boolean call() {
+                    try {
+                        sessionManager.deleteUser(toDelete, sessionManager.getCurrentUser());
+                        return true;
+                    } catch (noPermissionException e) {
+                        return false;
+                    }
+                }
+            };
+
+            deleteTask.setOnSucceeded(otherEvent -> {
+                if (deleteTask.getValue()) {
+                    System.out.println("user deleted");
+                    fillGUI();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Deletion Error");
+                    alert.setContentText("You do not have permission to delete this user");
+                    alert.showAndWait();
+                }
+            });
+
+            Thread thread = new Thread(deleteTask);
+            thread.setDaemon(true);
+            thread.start();
+        }
+
+        // try {
+        // sessionManager.deleteUser(toDelete, sessionManager.getCurrentUser());
+        // } catch (noPermissionException e) {
+        // System.out.println("User " + sessionManager.getCurrentUsername()
+        // + " does not have permission to delete " + toDelete.getUserName());
+        // }
     }
 
     @FXML
     void logout(ActionEvent event) {
-        // TODO: Logout
         windowManager.switchToScene("primaryStage", "loginScene");
     }
 
     @FXML
     void setAdmin(ActionEvent event) {
-        // TODO: Set user as admin
+        User toPromote = userList.getSelectionModel().getSelectedItem();
+
+        if (toPromote == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No user selected");
+            alert.setContentText("Please select a user to delete");
+            alert.showAndWait();
+        }
+
+        if (toPromote != null) {
+            System.out.println(sessionManager.getCurrentUsername() + " is promoting " + toPromote.getUserName());
+            System.out.println("Current user is " + sessionManager.getCurrentUser().getUserType());
+
+            Task<Boolean> promoteTask = new Task<>() {
+                @Override
+                protected Boolean call() {
+                    return sessionManager.changeUserType(toPromote, sessionManager.getCurrentUser(), UserType.ADMIN);
+                }
+            };
+
+            promoteTask.setOnSucceeded(otherEvent -> {
+                if (promoteTask.getValue()) {
+                    System.out.println("user promoted to admin");
+                    fillGUI();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Promotion Error");
+                    alert.setContentText("You do not have permission to promote this user to admin");
+                    alert.showAndWait();
+                }
+            });
+
+            Thread thread = new Thread(promoteTask);
+            thread.setDaemon(true);
+            thread.start();
+        }
     }
 
     @FXML
     void setPremium(ActionEvent event) {
-        // TODO: Set user as premium
+        User toPromote = userList.getSelectionModel().getSelectedItem();
 
+        if (toPromote == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No user selected");
+            alert.setContentText("Please select a user to delete");
+            alert.showAndWait();
+        }
+
+        if (toPromote != null) {
+            System.out.println(sessionManager.getCurrentUsername() + " is promoting " + toPromote.getUserName());
+            System.out.println("Current user is " + sessionManager.getCurrentUser().getUserType());
+
+            Task<Boolean> promoteTask = new Task<>() {
+                @Override
+                protected Boolean call() {
+                    return sessionManager.changeUserType(toPromote, sessionManager.getCurrentUser(),
+                            UserType.PREMIUM_USER);
+                }
+            };
+
+            promoteTask.setOnSucceeded(otherEvent -> {
+                if (promoteTask.getValue()) {
+                    System.out.println("user promoted to premium user");
+                    fillGUI();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Promotion Error");
+                    alert.setContentText("You do not have permission to promote this user to premium user");
+                    alert.showAndWait();
+                }
+            });
+
+            Thread thread = new Thread(promoteTask);
+            thread.setDaemon(true);
+            thread.start();
+        }
     }
 
     @Override
@@ -89,5 +219,7 @@ public class AdminController extends BasicController {
     @Override
     public void fillGUI() {
         userNameLabel.setText(sessionManager.getCurrentUsername());
+        userList.getItems().clear(); // Clear the list
+        userList.getItems().addAll(sessionManager.getUsers().values());
     }
 }
